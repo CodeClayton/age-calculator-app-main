41ed042f4c741a4b1e49bab39090e70352bb2d6e
Clayton Henrique <134659710+CodeClayton@users.noreply.github.com>
2025-09-26 10:04:17 -0300

    feat: implementação de toast para informações

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,20 +7,52 @@ const yearValue = document.querySelector('.yearValue')
 const monthValue = document.querySelector('.monthValue')
 const dayValue = document.querySelector('.dayValue')
 
+//Validators form
+dayInput.addEventListener('input',() => {
+  console.log(dayInput.value)
+})
+
+
 calcButton.addEventListener('click', () => {
   const d = Number(dayInput.value);
   const m = Number(monthInput.value);
   const y = Number(yearInput.value);
 
   if (!d || !m || !y) {
-    console.log('Preencha todos os campos.');
+      Toastify({
+        text: "Preencha todos os campos.",
+        duration: 3000,
+        backgroundColor:"#2196F3"
+        }).showToast();
     return;
+  }
+
+  let dayIsCorrect = verifyDay(d,m);
+  if(!dayIsCorrect){
+    Toastify({
+        text: "Data inválida.",
+        duration: 3000,
+        backgroundColor:"#F44336"
+        }).showToast();
+    return;
+  }
+
+  if(y < 1901 || m > 12){
+    Toastify({
+      text: 'Data inválida.',
+      duration: 3000,
+      backgroundColor:"#F44336"
+      }).showToast();
   }
 
   const birthDate = new Date(y, m - 1, d);
   const today = new Date();
   if (birthDate > today) {
-    console.log('Data no futuro não é válida.');
+        Toastify({
+        text: 'Data no futuro não é válida.',
+        duration: 3000,
+        backgroundColor:"#F44336"
+        }).showToast();
     return;
   }
 
@@ -29,7 +61,12 @@ calcButton.addEventListener('click', () => {
     birthDate.getMonth() !== m - 1 ||
     birthDate.getFullYear() !== y
   ) {
-    console.log('Data inválida.');
+  Toastify({
+    text: "Data inválida.",
+    duration: 3000,
+    backgroundColor:"#F44336"
+    }).showToast();
+
     return;
   }
 
@@ -57,4 +94,9 @@ function calculateAge(birthDate, today) {
   }
 
   return { years, months, days };
+}
+
+function verifyDay(day, month){
+  const date = new Date(2023, month - 1, day);
+  return date.getDate() === day && date.getMonth() === (month - 1);
 }
